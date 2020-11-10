f93f5e79cea78f29323e52072d8a46b4b190aa71
Marko Peitsila <marko.peitsila@gmail.com>
2020-11-10 22:59:44 +0200

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          JOULU BING0 !!! <p>*&lt;|:-)</p>
+          JOULU BING0 !! <p>*&lt;|:-)</p>
         </p>
       </header>
       <AmplifySignOut />
